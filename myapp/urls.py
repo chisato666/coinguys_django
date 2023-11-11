a17bfc98cd9c66990381a5234bdb4d72553e0685
chisato666 <34371312+chisato666@users.noreply.github.com>
2023-11-11 17:51:53 +0800

    Add files via upload

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
      path('check_alert', views.check_alert),
 
      path('show_alert', views.show_alert),
+     path('research', views.research),
+     path('submit_research', views.submit_research),
+
      path('', views.say_hello),
      path('getProfiles',views.getProfiles, name='getProfiles'),
      path('get_btcusdt_price', views.get_btcusdt_price, name='get_btcusdt_price'),
